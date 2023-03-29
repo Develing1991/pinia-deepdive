5267c28c0b104d696fb59f836e71cfb961e86541
Develing1991 <completed0728@gmail.com>
2023-03-29 17:42:41 +0900

    composable.. vueuse localstorage

--- stores/CartStore.js
+++ stores/CartStore.js
@@ -1,10 +1,12 @@
 import { defineStore, acceptHMRUpdate } from "pinia";
 import { groupBy } from "lodash";
 import { useAuthUserStore } from "./AuthUserStore";
+import { useLocalStorage } from "@vueuse/core";
+
 export const useCartStore = defineStore("CartStore", {
   historyEnabled: true,
   state: () => ({
-    items: [],
+    items: useLocalStorage("CartStore:items", []),
   }),
   getters: {
     // arrow function에서는 this는 사용불가
